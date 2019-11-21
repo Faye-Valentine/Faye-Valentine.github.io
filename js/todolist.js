27a4f5acca2e2c7b1e4ce8d12be8e9c3265c9888
Faye-Valentine <53758369+Faye-Valentine@users.noreply.github.com>
2019-11-21 17:23:24 +0800

    s

---
+++ js/todolist.js
@@ -0,0 +1,79 @@
+new Vue({
+  el: "#app",
+  data() {
+    return {
+      items: [
+        {
+          title: "每天吃早饭   (ง •_•)ง",
+          edit: false
+        },
+        {
+          title: "坚持每天不吃外卖  (≧∇≦)ﾉ",
+          edit: false
+        },
+        {
+          title: "每天都要睡足   精神满满哦(～￣▽￣)～",
+          edit: false
+        }
+      ],
+      taskItem: "",
+      isError: false
+    };
+  },
+  methods: {
+    addItem: function(e) {
+      value = e.target.value;
+
+      if (value === "") {
+        return false;
+      }
+
+      if (this.itemExist(value) === true) {
+        let repeated = [].filter.call(
+          document.getElementsByTagName("span"),
+          function(span) {
+            return span.textContent === value;
+          }
+        );
+        this.isError = true;
+        repeated[0].classList.add("bg-orange-300");
+        return false;
+      }
+
+      this.isError = false;
+      e.target.value = "";
+      this.items.push({
+        title: value,
+        edit: false
+      });
+
+      let repeatedClasses = document.querySelectorAll(".bg-orange-300");
+      for (let repeatedClass of repeatedClasses) {
+        repeatedClass.classList.remove("bg-orange-300");
+      }
+    },
+    removeItem: function(index) {
+      this.items.splice(index, 1);
+      this.$refs.addtask.focus();
+    },
+    enableEdit: function(index) {
+      this.items[index].edit = true;
+      this.$nextTick(() => {
+        let input = this.$refs.task[0];
+        input.focus();
+      });
+    },
+    disableEdit: function(index) {
+      this.items[index].edit = false;
+    },
+    itemExist: function(value) {
+      for (var i = 0; i < this.items.length; i++) {
+        if (this.items[i].title === value) {
+          return true;
+        }
+      }
+
+      return false;
+    }
+  }
+});
